1e882f55d73cbd12e37d7299155cedc86f6737b7
Munekatsu Furugen <rinconlincon@gmail.com>
2020-08-11 16:36:06 +0900

    Added handleChange and handleSubmit on Airline component

--- app/javascript/components/Airline/Airline.jsx
+++ app/javascript/components/Airline/Airline.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
 import Header from './Header'
 import ReviewForm from './ReviewForm'
@@ -43,26 +43,57 @@ const Airline = (props) => {
 
   }, [])
 
+  const handleChange = (e) => {
+    e.preventDefault()
 
+    const csrfToken = document.querySelector('[name=csrf-token]').content
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+    setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+
+    console.log('review', review)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const airline_id = airline.data.id
+    axios.post('/api/v1/reviews', {review, airline_id})
+    .then(resp => {
+      const included = [...airline.included, resp.data]
+      setAirline({...airline, included})
+      setReview({title: '', description: '', score: 0})
+    })
+    .catch(resp => {})
+  }
 
 
   return (
     <Wrapper>
-      <Column>
-        <Main>
-          {
-            loaded &&
-            <Header
+      {
+        loaded &&
+        <Fragment>
+          <Column>
+            <Main>
+
+                <Header
+                  attributes={airline.data.attributes}
+                  reviews={airline.included}
+                />
+
+              <div className="reviews"></div>
+            </Main>
+          </Column>
+          <Column>
+            <ReviewForm
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
               attributes={airline.data.attributes}
-              reviews={airline.included}
+              review={review}
             />
-          }
-          <div className="reviews"></div>
-        </Main>
-      </Column>
-      <Column>
-        <div className="review-form">[Review Form Goes Here.]</div>
-      </Column>
+          </Column>
+        </Fragment>
+      }
     </Wrapper>
   )
 }
